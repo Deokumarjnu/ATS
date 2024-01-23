3e2b2ddb46ea43f30bada234df948238912e5df1
Deo.kumar <deo.kumar@powerschool.com>
2024-01-23 16:52:23 +0530

    Updated prompts

--- app.py
+++ app.py
@@ -25,23 +25,6 @@ def convert_pdf_to_text(uploaded_file):
         text+=str(page.extract_text())
     return text
 
-# Prompt temapltes
-input_prompt="""
-Hey Act Like a skilled or very experience ATS(Application Tracking System)
-with a deep understanding of tech field,software engineering,data science ,data analyst
-and big data engineer. Your task is to evaluate the resume based on the given job description.
-You must consider the job market is very competitive and you should provide 
-best assistance for improving thr resumes. Assign the percentage Matching based 
-on Jd and
-the missing keywords with high accuracy
-resume:{text}
-description:{jd}
-
-I want the response in one single string having the structure
-{{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
-"""
-
-
 ## streamlit app
 st.title("Smart ATS")
 st.text("Improve Your Resume ATS")
@@ -51,7 +34,29 @@ uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please upla
 submit = st.button("Submit")
 
 if submit and uploaded_file is not None:
-    text=convert_pdf_to_text(uploaded_file)
-    response=get_gemini_response(input_prompt)
+    resume=convert_pdf_to_text(uploaded_file)
+    # Prompt temapltes
+    formattedPrompt = '''
+       Follow the instructions carefully to conduct a meticulous comparison between the job description and the provided resume within the specified context, adhering to the outlined style guidelines.
+
+        **INSTRUCTIONS:**
+        As a proficient ATS (Applicant Tracking System) scanner with a deep understanding of any provided job description and ATS functionality, your task is to thoroughly evaluate the resume against the provided job description.
+
+        **CONTEXT:**
+        Provide the percentage match if the resume aligns with the job description, focusing specifically on the required technical skills outlined in the job description: {jd}.
+
+        **GUIDELINES:**
+        1. Responses should directly address both the job description: {jd} and the resume: {resume}.
+        2. Pay special attention to the Technical Skills Required section of the job description: {jd} to ensure a precise match with the resume: {resume}.
+        3. If Resume does not explicitly mention any skills listed in the missing keywords Required section.
+        4. The response should be in the form of a percentage match, identification of missing keywords related to technical skills, and final thoughts.
+        5. Response percentage should be based on the skills in the resume: {resume} that match the job description: {jd}.
+
+    '''
+
+    # Using format method to substitute values
+    formatted_string = formattedPrompt.format(jd=jd, resume=resume)
+
+    response=get_gemini_response(formatted_string)
     st.subheader(response)
     
